904f12a2cc7d6426aaf72ce04f3512059668c302
Brandoi008 <159958377+Brandoi008@users.noreply.github.com>
2024-04-27 18:21:12 +0000

    Added Css

--- django_project/views.py
+++ django_project/views.py
@@ -3,12 +3,13 @@ import requests
 from django.shortcuts import render
 
 def home(request):
-    response = requests.get('https://api.github.com/events')
+    response = requests.get('https://official-joke-api.appspot.com/random_joke')
     data=response.json()
-    result=data[0]['repo']
+    result=data['setup']
+    result1=data['punchline']
 
     response2=requests.get('https://dog.ceo/api/breeds/image/random')
     data2 =response2.json()
     result2 =data2['message']
-    return render(request, 'templates/index.html',{'result':result,'result2':result2})
+    return render(request, 'templates/index.html',{'result':result, 'result1':result1,'result2':result2})
   
