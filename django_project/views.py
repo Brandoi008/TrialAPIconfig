476486e95da1e6d5f31ca9517e5f2f67f3e82f70
Brandoi008 <159958377+Brandoi008@users.noreply.github.com>
2024-04-27 17:10:52 +0000

    Added API and incorporated in html file

--- django_project/views.py
+++ django_project/views.py
@@ -1,5 +1,14 @@
+from django.http import response
+import requests
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'templates/index.html')
+    response = requests.get('https://api.github.com/events')
+    data=response.json()
+    result=data[0]['repo']
+
+    response2=requests.get('https://dog.ceo/api/breeds/image/random')
+    data2 =response2.json()
+    result2 =data2['message']
+    return render(request, 'templates/index.html',{'result':result,'result2':result2})
   
